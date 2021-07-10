9d493265ed500721ddd62f2227a86a573e7f0e6e
Marcelo Mena <marcelomena010@gmail.com>
2021-07-09 22:47:44 -0300

    feat: flex button

--- src/components/ui/Button/styles.ts
+++ src/components/ui/Button/styles.ts
@@ -2,8 +2,14 @@ import styled from 'styled-components'
 import { lighten } from 'polished'
 
 export const Container = styled.button`
+  cursor: pointer;
+
   background-color: ${({ theme: { colors } }) => colors.white};
   border: none;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   &:disabled {
     background-color: ${({ theme: { colors } }) => lighten(0.1, colors.white)};
